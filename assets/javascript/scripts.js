0c34800670fffbc3c02c02ec824c562da5e18502
tavinogradova92 <tavinogradova92@gmail.com>
2019-10-31 21:38:29 +0100

    Added json fetch reader

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -61,51 +61,40 @@ timeButton.addEventListener("click", function() {
 });
 
 //Showing today's task
-
 let showTaskButton=document.getElementById("ToDo");
-
 let weekdays=days[currentDatetime.getDay()];
-
-// -- I failed to read local file, see bellow functions I tried :-(
-//-- Hope to fix it after hosting on github
-var insertedTasks = '[{"id": 1,"day": "Monday","time": "08:00","description": "Laugh to Your Heart’s Content"},{"id": 2,"day": "Monday","time": "11:00","description": "Dance Like No One’s Watching"},{"id": 3,"day": "Monday","time": "16:00","description": "Make Dinner for homeless guy"},{"id": 4,"day": "Tuesday","time": "07:00","description": "Change Your Morning Routine"},{"id": 5,"day": "Tuesday","time": "10:00","description": "Go People Watching"},{"id": 6,"day": "Tuesday","time": "18:00","description": "Do Something Stupid"},{"id": 7,"day": "Wednesday","time": "08:00","description": "Sing in the Shower"},{"id": 8,"day": "Wednesday","time": "13:00","description": "Listen to Nature Sounds"},{"id": 9,"day": "Wednesday","time": "15:00","description": "Join a Club"},{"id": 10,"day": "Thursday","time": "06:00","description": "Strike a Pose"},{"id": 11,"day": "Thursday","time": "09:00","description": " Challenge Yourself"},{"id": 12,"day": "Thursday","time": "20:00","description": "Reconnect with an Old Friend"},{"id": 13,"day": "Friday","time": "09:00","description": "Learn a Joke"},{"id": 14,"day": "Friday","time": "12:00","description": "Become an Expert"},{"id": 15,"day": "Friday","time": "19:00","description": "Take a Class"},{"id": 16,"day": "Saturday","time": "07:00","description": "Change Your Look"},{"id": 17,"day": "Saturday","time": "12:00","description": "Excite Your Palate"},{"id": 18,"day": "Saturday","time": "21:00","description": "Begin a Diary"},{"id": 19,"day": "Sunday","time": "10:00","description": "Embrace Your Curiosity"},{"id": 20,"day": "Sunday","time": "11:00","description": "Pretend to Be a Tourist"},{"id": 21,"day": "Sunday","time": "17:00","description": "Drive Away with No Destination in Mind"}]';
-const json=JSON.parse(insertedTasks);
-let todaysTasks = json.filter(task => task.day === weekdays);
-//console.log(todaysTasks);
-
-
-let paragraphToChange=document.getElementById("whatTask");
-let createTable = document.createElement("table");
-
 showTaskButton.addEventListener("click", function() {
     let table = document.createElement('table');
     table.setAttribute('border','0');
     table.setAttribute('width','100%');
-
     let header = table.createTHead();
     let row = header.insertRow(0);    
     let cell = row.insertCell(0);
     cell.innerHTML = `<b>Your tasks on ${weekdays}</b>`;
-
-    for (let x in todaysTasks) {
-       row = table.insertRow(x);
-
-       cell = row.insertCell();
-       cell.setAttribute('align','left');
-       text = document.createTextNode(todaysTasks[x].description);
-       cell.appendChild(text);
     
-       cell = row.insertCell();
-       cell.setAttribute('align','left');
-       text = document.createTextNode(todaysTasks[x].time);
-       cell.appendChild(text);
-    }
+    fetch('assets/javascript/tasks.json')
+    .then(response => response.json())
+    .then(tasks =>{
+         let todaysTasks = tasks.filter(task => task.day === weekdays);
+         console.log(todaysTasks);
+        for (let x in todaysTasks) {
+            row = table.insertRow(x);
+            cell = row.insertCell();
+            cell.setAttribute('align','left');
+            text = document.createTextNode(todaysTasks[x].description);
+            cell.appendChild(text);
+            cell = row.insertCell();
+            cell.setAttribute('align','left');
+            text = document.createTextNode(todaysTasks[x].time);
+            cell.appendChild(text);
+            }
+            document.getElementById("whatTask").appendChild(table);
+        })
+    .catch(error   => console.error(error));
 
-    document.getElementById("whatTask").appendChild(table);
 });
 
 // Title changer
-
 let changeTitleButton=document.getElementById("newTitle");
 changeTitleButton.addEventListener("click", function(event) {
     let new_title=document.getElementById("ourField").value;
@@ -123,33 +112,3 @@ colorSelector.addEventListener("change", function() {
     let new_bc=document.getElementById("colorSelector").value;
     document.body.style.background = new_bc;
 });
-
-//------------------------------------------------
-// Functions I tried to load local JSON 
-// but failed to do it locally
-//------------------------------------------------
-function load_json_fetch() {
-    fetch('tasks.json', {mode: 'no-cors'})
-    .then(response => response.text())
-    .then(data=> console.log(data))
-    .catch(error => console.error(error));
-}
-//  Error
-//  Fetch API cannot load file:///D:/docs/Tanya/Frontend/Wiredelta/assets/javascript/tasks.json. URL scheme "file" is not supported.
-
-function load_json_XmlReq(callback) {   
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'tasks.json', true); 
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
- //  Error
-//  read_json_demo.js:29 Access to XMLHttpRequest at 'file:///D:/docs/Tanya/Frontend/Wiredelta/assets/javascript/tasks.json' from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
-
-
